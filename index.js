5552cc6beeed222517e3fd4362c0b03d68d5142a
RITA-HITECH <ritabindal63@gmail.com>
2021-07-31 16:36:20 +0530

    variables and functions in javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+//object
+let person = {
+
+
+name: 'rita bindal',
+age : 23
+
+};
+person.name = "jon";
+console.log(person.name);
+
+//arrays
+
+let colors = ['red','green','blue'];
+colors[3]='brown';
+console.log(colors.length); //for finding how many elements are there in array
+
+//functions(performing a task)
+
+function greet(name){  //declaration
+    console.log("Hello"+" "+name);
+
+}
+greet('Rita');   //calling
+greet('Aashi');
+
+//functions(calculating a value)
+
+function square(number){
+    return number * number;
+}
+
+console.log(square(5));
+
+
+
+
+
+
