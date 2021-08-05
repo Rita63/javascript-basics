7dcba00a680d16877a19b47bfd939a465ce6d28a
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 18:14:40 +0530

    for loop

---
+++ for.js
@@ -0,0 +1,20 @@
+//print even number from 1 to 50
+
+for(let i=2;i<=50;i++)
+{
+    
+    if(i%2===0)
+
+
+    console.log(i);
+}
+
+//To get number in reverse order
+
+for(let i=30; i>=2; i--)
+{
+    if(i%2===0)
+    console.log(i);
+
+}
+
