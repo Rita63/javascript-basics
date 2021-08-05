eb6e9c9b1339adcd016b445484ecbe7ab6de350a
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 18:45:12 +0530

    infinite loop

---
+++ infinite loop.js
@@ -0,0 +1,30 @@
+//1st case like if we foegot to do increment
+
+let k=2;
+while(k<=30){
+console.log(k)
+
+//i++;
+}
+
+
+//2nd case
+
+while(true){
+
+}
+
+//3rd case
+
+let x=0;
+do{
+    //x++;
+    console.log(x);
+
+}while(x>=5);
+
+
+//4th case
+
+for(let y=0;y<10; )
+
