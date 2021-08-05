3e104858b9b22e126ae2ffa3f5eb635cc8233ab6
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 19:33:28 +0530

    problem1

---
+++ javascript exercises/problem1.js
@@ -0,0 +1,12 @@
+// write a fun that takes two numbers and returns the maximum
+
+
+function max(a,b)
+{
+    if(a>b)
+    return a;
+    else 
+    return b;
+}
+let number =max(6,9);   //calling
+console.log(number)
