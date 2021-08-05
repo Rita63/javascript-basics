759bd3bab7b3c8ca106a61f39be6d6d6ad9788f9
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 18:36:41 +0530

    do-while loop

---
+++ do-while.js
@@ -0,0 +1,10 @@
+//print table of 2 using d0-while loop 
+
+
+let j=2;               //initialization
+do{
+    if(j%2===0)
+    console.log(j);
+    j++;                //increment
+
+}while(j<=30);           //condition
