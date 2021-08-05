4278cc6a24eeedf6cf9520d58c2bf6cf77ce4126
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 20:31:23 +0530

    problem5

---
+++ javascript exercises/problem5.js
@@ -0,0 +1,12 @@
+
+
+//to count the truthy value in array
+const array= [0,null,undefined,'',2,3]
+function countTruthy(array){
+let count =0;
+for(let value of array)
+if(value)
+count ++;
+return count;
+}
+console.log(countTruthy(array));
