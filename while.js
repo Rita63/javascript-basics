d91b5ba7b2105a4b7d8acae14dbe370c862380e0
RITA-HITECH <ritabindal63@gmail.com>
2021-08-05 18:24:15 +0530

    while loop

---
+++ while.js
@@ -0,0 +1,11 @@
+//print the table of 2 upto a given range using while loop
+
+
+let i=2;               //initialization
+while(i<=30)          //condition
+{
+    if(i%2===0)
+    console.log(i);
+    i++;              //increment
+
+}
